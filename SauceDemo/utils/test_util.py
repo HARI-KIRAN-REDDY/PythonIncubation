e48d090a7a50cd8dc156a78653f340a41607e633
HARI-KIRAN-REDDY <lokeshreddydevireddy@gmail.com>
2023-10-25 12:04:38 +0530

    requirements.txt added

--- SauceDemo/utils/test_util.py
+++ SauceDemo/utils/test_util.py
@@ -3,12 +3,24 @@ from selenium import webdriver
 from utils.config_util import ConfigUtil
 # from my_pages.my_pages import LoginPage
 from pages.login_page import LoginPage
+from selenium.webdriver.chrome.options import Options as ChromeOptions
 
 
 @pytest.fixture(params=ConfigUtil.get_browsers())
 def get_browser(request):
     driver = None
     if request.param == 'chrome':
+        # options = ChromeOptions()
+        # options.browser_version = 'latest'
+        # options.platform_name = 'Windows 11'
+        # sauce_options = {}
+        # sauce_options['username'] = 'oauth-lokeshreddydevireddy-f3dab'
+        # sauce_options['accessKey'] = '35bd94bd-0b74-4acf-a1ff-264ce0d65cf6'
+        # sauce_options['build'] = 'selenium-build-WP4M7'
+        # sauce_options['name'] = '<your test name>'
+        # options.set_capability('sauce:options', sauce_options)
+        # url = "https://ondemand.eu-central-1.saucelabs.com:443/wd/hub"
+        # driver = webdriver.Remote(command_executor=url, options=options)
         driver = webdriver.Chrome()
     elif request.param == 'edge':
         driver = webdriver.Edge()
